f37e06fb76243864babafc02d25fb785ecf59f93
Tobias Kündig <tobias@offline.ch>
2021-01-25 08:34:03 +0100

    Fixed factories/factory naming mess

--- classes/MultiFactory.php
+++ classes/MultiFactory.php
@@ -13,9 +13,9 @@ class MultiFactory extends Factory
      *
      * @return static
      */
-    public static function constructMany(Faker $faker, string $pattern)
+    public static function constructMany(Faker $faker)
     {
-        return (new static($faker))->loadMany($pattern);
+        return (new static($faker))->loadMany();
     }
 
     /**
@@ -23,11 +23,13 @@ class MultiFactory extends Factory
      *
      * @return $this
      */
-    public function loadMany(string $pattern)
+    public function loadMany()
     {
         $factory = $this;
 
-        foreach (Finder::create()->files()->name($pattern)->in(plugins_path()) as $file) {
+        $files = Finder::create()->files()->name('factory.php')->name('factories.php')->in(plugins_path());
+
+        foreach ($files as $file) {
             require $file->getRealPath();
         }
 
